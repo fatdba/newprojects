76bde80fa903f59949a61493deecbb53406174e9
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2025-04-24 14:46:58 -0400

    Update stats_howold.sql

--- stats_howold.sql
+++ stats_howold.sql
@@ -19,6 +19,29 @@ ORDER BY
 
 ==========
 
+    SELECT 
+    owner, 
+    table_name, 
+    num_rows, 
+    blocks, 
+    TO_CHAR(last_analyzed, 'DD-MON-YY') AS last_analyzed,
+    CASE 
+        WHEN last_analyzed IS NOT NULL AND last_analyzed > SYSDATE - 31 THEN 'FRESH'
+        ELSE 'STALE'
+    END AS stats,
+    TRUNC(SYSDATE - last_analyzed) AS days_since_last_analyzed,
+    partitioned
+FROM 
+    dba_tables
+WHERE 
+    owner IN ('CSUSER', 'NNUSER')
+ORDER BY 
+    owner, table_name;
+
+
+
+==========
+
 SELECT owner, COUNT(*) AS stale_table_count
 FROM (
   SELECT owner,
